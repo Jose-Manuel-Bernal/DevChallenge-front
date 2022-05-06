72e58c20e44eda9c730be1e1de2f2d84a06569e9
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-05-06 11:50:42 -0500

    Category component created and its basics operations. This component will create the new Category List

---
+++ src/components/Category.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useRef } from "react";
+
+const Category = () => {
+  const formRef = useRef(null);
+
+  const [title, setTitle] = useState("");
+
+  const newList = (event) => {
+    event.preventDefault();
+    formRef.current.reset();
+  };
+
+  const addingTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <form ref={formRef}>
+      <input
+        onChange={addingTitle}
+        type="text"
+        name="title"
+        placeholder="To Do List"
+      />
+      <button onClick={newList}>New List</button>
+    </form>
+  );
+};
+
+export default Category;
